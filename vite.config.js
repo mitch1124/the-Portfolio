e09f618a16fc8a0d4112c0f27d2279bc95679ed2
Mitch Lindsey <mrl314@ensign.edu>
2026-05-12 10:30:54 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
-import path from "path"
+//import tailwindcss from "@tailwindcss/vite";
+//import path from "path"
 
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/the-Portfolio/',
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  //resolve: {
+    //alias: {
+      //"@": path.resolve(__dirname, "./src"),
     },
   }
 });
